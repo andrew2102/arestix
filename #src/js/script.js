ae2a9eb1351238e20824054c759f484089dbe656
Andrey Osadchiy <andrei210284@rambler.ru>
2021-03-02 21:52:44 +0200

    Add Sliders x2

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,3 +1,21 @@
 @@include('functions.js', {});
+@@include('slick.min.js', {});
+
+// Slider
+$('.customer__slider').slick({
+   arrows: false,
+   dots: true,
+   slidesToShow: 1,
+   easing: 'ease',
+});
+
+$('.screenshots__slider').slick({
+   arrows: false,
+   dots: true,
+   slidesToShow: 4,
+   slidesToScroll: 2,
+   easing: 'ease',
+   touchThreshold: 10,
+});
 
 console.log('Hello!');
